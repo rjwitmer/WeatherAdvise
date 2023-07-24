2eef90a8a4335386aae526a90ceb273fe3ddea6d
Bob Witmer <bwitmer@golden.net>
2023-07-24 11:44:14 -0400

    Initial Commit

---
+++ WeatherAdvise/WeatherAdviseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAdviseApp.swift
+//  WeatherAdvise
+//
+//  Created by Bob Witmer on 2023-07-24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAdviseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
